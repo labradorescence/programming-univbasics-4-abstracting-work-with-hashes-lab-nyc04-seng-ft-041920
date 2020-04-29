100914d08fddd1a1d91c1ed7454014627440d97b
Greem <orangenewyorkcreativeagency@gmail.com>
2020-04-29 17:42:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,10 +3,10 @@ def my_hash_creator(key, value)
     {key => value}
 
 end
-
-
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  {key => hash}
+
 end
 
 def update_counting_hash(hash, key)
